fd140eb90c49388234115c450b111e436b24aadd
procode3 <wr.t8r1@gmail.com>
2023-02-28 14:30:00 +0300

    Updated parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		NewNode->left = parent->left;
 		parent->left = NewNode;
-		prent->left->left->parent = NewNode;
+		parent->left->left->parent = NewNode;
 	}
 	return (NewNode);
 }
